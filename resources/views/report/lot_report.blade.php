e7dd42de1c381d0f7bd47a02a8f4eac62305c0cc
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-02-25 16:07:42 -0500

    listo

--- resources/views/report/lot_report.blade.php
+++ resources/views/report/lot_report.blade.php
@@ -109,8 +109,9 @@
                             <th>@lang('report.current_stock')</th>
                             <th>@lang('report.total_unit_sold')</th>
                             <th>@lang('lang_v1.total_unit_adjusted')</th>
-                            <th>Fecha de pago</th>
+                            
                             <th>Acciones</th>
+                            <th>Fecha de pago</th>
                         </tr>
                     </thead>
                     <tfoot>
